b56b9c0d4a3bfb64c9c5033b36f24064db079416
Smack <Smack@Macintosh.local>
2018-02-16 12:19:20 -0500

    added update/put  to orm/controllers

--- config/orm.js
+++ config/orm.js
@@ -3,6 +3,9 @@ var connection = require("../config/connection.js");
 var orm = {
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
+        console.log(queryString);
+
+        
         connection.query(queryString, function(err, result){
             if (err) {
                 throw err;
@@ -13,6 +16,9 @@ var orm = {
     
     create: function(table, vals, cb) {
         var queryString = "INSERT INTO " + table + " (burger_name) VALUES ('" + vals + "');"
+        console.log(queryString);
+
+        
         connection.query(queryString, vals, function(err, result) {
             if (err) {
                 throw err;
@@ -21,10 +27,29 @@ var orm = {
         });
     },
 
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table + " SET devoured WHERE ";
+
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+
+    },
+
     delete: function(table, condition, cb) {
         var queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
         queryString += condition;
+
+        console.log(queryString);
 
         connection.query(queryString, function(err, result) {
             if (err) {
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -24,6 +24,22 @@ router.post("/api/burgers", function(req, res){
     });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows ==0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 
 router.delete("/api/burgers/:id", function(req,res) {
     var condition = "id = " + req.params.id;
