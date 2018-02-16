ed50d0fe1998be321f1d9dc1d19b07285fbe8738
Smack <Smack@Macintosh.local>
2018-02-16 11:19:32 -0500

    routes working, getting data objects

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,7 +6,7 @@ var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.all(function(data) {
-        var = hbsObj = {
+        var hbsObj = {
             burgers: data
         };
         console.log(hbsObj);
@@ -18,7 +18,7 @@ router.post("/api/burgers", function(req, res){
     burger.create([
         "burger_name"
     ], [
-        req.bidy.burger_name
+        req.body.burger_name
     ], function(result) {
         res.json({ id: result.insertId});
     });
